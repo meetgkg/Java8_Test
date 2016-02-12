513be6b5c9677741e438d3ec680cc222db601854
Gaurav Gupta <meetgkg@gmail.com>
2016-02-11 21:22:29 -0500

    New code

---
+++ src/com/java8/chap3/IntPredicateEx.java
@@ -0,0 +1,13 @@
+package com.java8.chap3;
+
+import java.util.function.IntPredicate;
+
+public class IntPredicateEx {
+
+	public static void main(String[] args) {
+		IntPredicate evenNum = (int i) -> i%2 == 0;
+		System.out.println(evenNum.test(123));
+	}
+	
+
+}
